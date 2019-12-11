c64341ed0c05cecedfcc7410ff2c744320686150
huangtao <huangtao@ifclover.com>
2019-12-11 15:40:26 +0800

    New version: v1.0.4

--- aioquant/__init__.py
+++ aioquant/__init__.py
@@ -9,7 +9,7 @@ Email:  huangtao@ifclover.com
 """
 
 __author__ = "HuangTao"
-__version__ = (1, 0, 3)
+__version__ = (1, 0, 4)
 
 
 from aioquant.quant import AIOQuant
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="aioquant",
-    version="1.0.3",
+    version="1.0.4",
     packages=[
         "aioquant",
         "aioquant.utils",
